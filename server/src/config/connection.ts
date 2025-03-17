c6bb3d0a668af6bafb5d3bfbe5dea87580b17dfb
unknown <carterpaccione@gmail.com>
2025-03-17 16:36:36 -0400

    connect to postgresql test

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,19 +3,26 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = process.env.DB_URL
-    ? new Sequelize(process.env.DB_URL)
-    : new Sequelize(
-        process.env.DB_NAME || "",
-        process.env.DB_USER || "",
-        process.env.DB_PASSWORD || "",
-        {
-            host: process.env.DB_HOST || "localhost",
-            dialect: "postgres",
-            dialectOptions: {
-                decimalNumbers: true,
-            },
-        }
-    );
+let sequelize;
+
+if (process.env.DB_URL) {
+    sequelize = new Sequelize(process.env.DB_URL);
+} else {
+    sequelize = process.env.DB_URL
+        ? new Sequelize(process.env.DB_URL)
+        : new Sequelize(
+            process.env.DB_NAME || "",
+            process.env.DB_USER || "",
+            process.env.DB_PASSWORD || "",
+            {
+                host: process.env.DB_HOST || "localhost",
+                dialect: "postgres",
+                dialectOptions: {
+                    decimalNumbers: true,
+                },
+            }
+        );
+
+}
 
 export default sequelize;
